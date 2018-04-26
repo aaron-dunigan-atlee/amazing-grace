4a82024c8dac8a5a1c24b84a28ff3a1b0e6cd5bf
Mr. D.A <sturgisMrDA@users.noreply.github.com>
2018-04-25 20:55:08 -0400

    Add loads of content and responsive design for header

---
+++ resources/scripts/menu.js
@@ -0,0 +1,29 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction(id) {
+  document.getElementById(id).classList.toggle("show");
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if ((openDropdown.id != id) && openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+      
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
