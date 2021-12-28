af2b12b1e8dc15557875d448632a5e8c3c309546
Aaron Dunigan AtLee <aaron.dunigan.atlee@gmail.com>
2021-12-28 10:06:42 -0700

    v1.0

--- resources/scripts/loadsheet.js
+++ resources/scripts/loadsheet.js
@@ -1,35 +1,51 @@
-var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1mI0Prj20RwR-9bMV_wnmKgavspKl7jg6cvYrOLi61Fg/edit?usp=sharing';
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1mI0Prj20RwR-9bMV_wnmKgavspKl7jg6cvYrOLi61Fg/pub?output=csv';
 
-function init() {
-Tabletop.init( { key: publicSpreadsheetUrl,
-                    callback: addToPage,
-                    simpleSheet: true } )
-}
-
-function logData(data, tabletop) {
-    console.log(data);
-}
-
-function addToPage(data, tabletop) {
-    var div = document.getElementById('gdoc-content');
-    var pageName = getMeta("page-name");
-    for (var i=0; i < data.length; i++) {
-        if (data[i].page === pageName) {
-            div.innerHTML += data[i].content;
-        }
+function init()
+{
+  // 8.15.21 Switched from tabletop to Papa Parse as tabletop is now broken
+  // See https://github.com/jsoma/tabletop
+  // and https://www.papaparse.com/docs
+  Papa.parse(publicSpreadsheetUrl, {
+    download: true,
+    header: true,
+    complete: function (results)
+    {
+      addToPage(results.data)
     }
+  })
 }
 
-function getMeta(metaName) {
-    const metas = document.getElementsByTagName('meta');
-  
-    for (let i = 0; i < metas.length; i++) {
-      if (metas[i].getAttribute('name') === metaName) {
-        return metas[i].getAttribute('content');
-      }
+function logData(data)
+{
+  console.log(data);
+}
+
+function addToPage(data)
+{
+  var div = document.getElementById('gdoc-content');
+  var pageName = getMeta("page-name");
+  for (var i = 0; i < data.length; i++)
+  {
+    if (data[i].page === pageName)
+    {
+      div.innerHTML += data[i].content;
     }
-  
-    return '';
   }
-  
+}
+
+function getMeta(metaName)
+{
+  const metas = document.getElementsByTagName('meta');
+
+  for (let i = 0; i < metas.length; i++)
+  {
+    if (metas[i].getAttribute('name') === metaName)
+    {
+      return metas[i].getAttribute('content');
+    }
+  }
+
+  return '';
+}
+
 window.addEventListener('DOMContentLoaded', init)
